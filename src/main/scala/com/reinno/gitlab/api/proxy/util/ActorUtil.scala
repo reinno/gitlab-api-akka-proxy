40e48b5594ac31c1cefbc48165a90b1dcacfc071
reinno <reinno@126.com>
2016-04-17 23:45:19 +0800

    add Actor Util

---
+++ src/main/scala/com/reinno/gitlab/api/proxy/util/ActorUtil.scala
@@ -0,0 +1,14 @@
+package com.reinno.gitlab.api.proxy.util
+
+import scala.concurrent.{ExecutionContext, Future}
+
+import akka.actor.ActorRef
+import akka.pattern.ask
+
+
+object ActorUtil {
+  def askActor[T](actor: ActorRef, msg: Any)(implicit ex: ExecutionContext): Future[T] = {
+    implicit val timeout = Constants.FUTURE_TIMEOUT
+    (actor ? msg).asInstanceOf[Future[T]]
+  }
+}
--- src/main/scala/com/reinno/gitlab/api/proxy/util/Constants.scala
+++ src/main/scala/com/reinno/gitlab/api/proxy/util/Constants.scala
@@ -1,5 +1,7 @@
 package com.reinno.gitlab.api.proxy.util
 
+import java.util.concurrent.TimeUnit
+
 object Constants {
   val REST_VERSION = "v3"
   val GITLAB_HOST = "gitlab.com"
@@ -7,4 +9,6 @@ object Constants {
 
   val SERVER_PORT = 8001
   val SERVER_HOST = "0.0.0.0"
+
+  val FUTURE_TIMEOUT = akka.util.Timeout(15, TimeUnit.SECONDS)
 }
